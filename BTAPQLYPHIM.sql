b9152492949822a0d1360afc7b28c523a7095593
Nguyễn Mạnh Linh <lmnguyen.work@gmail.com>
2023-02-13 01:17:14 +0700

    lam them btvn

--- BTAPQLYPHIM.sql
+++ BTAPQLYPHIM.sql
@@ -3,25 +3,17 @@ create database QuanLyPhim
 
 use QuanLyPhim
 
-CREATE TABLE Cinemas (
-	maRap varchar(255) not null primary key,
-	tenRap varchar(255)
-);
-
-INSERT INTO Cinemas VALUES ('CGV','CGV Cinemas')
-
-CREATE TABLE Users1( --drop table Users
+CREATE TABLE Users1( --drop table Users1
 	IdNguoiDung int not null primary key,
 	hoTen varchar(255),
 	sdt varchar(255),
 	gioiTinh varchar(255),
-	maRap varchar(255)
-	foreign key (maRap) references Cinemas(maRap)
 );
 
-INSERT INTO Users1 VALUES (1,'Nguyen Manh Linh','0971968611','Nam','CGV'), (2,'Dinh Tien Hoang','0123456789','Nam','CGV'), (3, 'Le Dinh Linh', '0987654321', 'Nam','CGV')
+INSERT INTO Users1 VALUES (1,'Nguyen Manh Linh','0971968611','Nam'), (2,'Dinh Tien Hoang','0123456789','Nam'), (3, 'Le Dinh Linh', '0987654321', 'Nam')
+INSERT INTO Users1 VALUES (4,'Nguyen Van A', '000000000', 'Nam'), (5,'Nguyen Van B', '000000000', 'Nam'), (6,'Nguyen Thi C', '000000000', 'Nu')
 
-CREATE TABLE Category1( --drop database Category
+CREATE TABLE Category1( --drop table Category1
 	IdDanhMuc varchar(255) not null primary key,
 	tenDanhMuc varchar(255)
 );
@@ -29,7 +21,7 @@ CREATE TABLE Category1( --drop database Category
 INSERT INTO Category1 VALUES ('PH','Phim Hot'),
 ('PBMCP','Phim Bo Moi Cap Nhat'), ('PHH','Phim Hoat Hinh'), ('DYT','Duoc Yeu Thich')
 
-CREATE TABLE Movie1( --drop table Movie
+CREATE TABLE Movie1( --drop table Movie1
 	IdPhim varchar(255) not null primary key,
 	tenPhim varchar(255),
 	theLoai varchar(255),
@@ -37,8 +29,7 @@ CREATE TABLE Movie1( --drop table Movie
 	ngaySanXuat int,
 	giaPhim int,
 	maRap varchar(255),
-	foreign key (IdDanhMuc) references Category1(IdDanhMuc),
-	foreign key (maRap) references Cinemas(maRap)
+	foreign key (IdDanhMuc) references Category1(IdDanhMuc)
 );
 
 INSERT INTO Movie1 VALUES ('#trolley','TROLLEY','Tam ly - Tinh cam', 'PH', 2022, 80000,'CGV'),
@@ -47,23 +38,26 @@ INSERT INTO Movie1 VALUES ('#trolley','TROLLEY','Tam ly - Tinh cam', 'PH', 2022,
 ('#nbm','NHA BA NU', 'Tam ly - Hai huoc','PH' ,2022, 150000,'CGV')
 
 
-CREATE TABLE Rent1( --drop table Rent
+CREATE TABLE Rent1( --drop table Rent1
 	IdVeThue varchar(255) not null primary key,
 	thoiGianThue datetime,
 	IdNguoiDung int,
 	IdPhim varchar(255),
 	IdDanhMuc varchar(255),
-	soTien int,
+	giaThue1Ngay int,
 	thoiGianTra datetime,
 	foreign key (IdDanhMuc) references Category1(IdDanhMuc),
 	foreign key (IdPhim) references Movie1(IdPhim),
 	foreign key (IdNguoiDung) references Users1(IdNguoiDung)
 );
 
-INSERT INTO Rent1 VALUES ('VT1','2023/02/05 00:00',1, '#op','PHH' ,200000, '2023/02/11 00:00'),
+INSERT INTO Rent1 VALUES ('VT1','2023/02/05 00:00',1, '#op','PHH' ,250000, '2023/02/11 00:00'),
 ('VT2','2023/02/05 00:00',2, '#nbm', 'PH' , 300000, '2023/02/11 00:00'),
-('VT3','2023/02/05 00:00',1, '#rm','DYT', 400000, '2023/02/11 00:00'),
-('VT4','2023/02/05 00:00',3, '#trolley', 'PBMCP' , 250000, '2023/02/11 00:00')
+('VT3','2023/02/05 00:00',2, '#rm','DYT', 400000, '2023/02/11 00:00'),
+('VT4','2023/02/05 00:00',3, '#trolley', 'PBMCP' , 250000, '2023/02/11 00:00'),
+('VT5','2023/02/05 00:00',1, '#nbm', 'PH' , 300000, '2023/02/11 00:00'),
+('VT6','2023/02/05 00:00',4, '#nbm', 'PH' , 300000, '2023/02/11 00:00'),
+('VT7','2023/02/05 00:00',5, '#nbm', 'PH' , 300000, '2023/02/11 00:00')
 
 
 SELECT * FROM Users1
@@ -71,9 +65,24 @@ SELECT * FROM Category1
 SELECT * FROM Movie1
 SELECT * FROM Rent1
 
-SELECT hoTen, tenPhim, theLoai, soTien, thoiGianThue, thoiGianTra
-FROM Users1
-inner join Rent1 ON Users1.IdNguoiDung = Rent1.IdNguoiDung
-inner join Movie1 ON Rent1.IdPhim = Movie1.IdPhim
-WHERE soTien <= 300000
-AND theLoai != 'Hanh dong'
+--a. Lay ra 3 nguoi thue nhieu phim nhat. Hien thi thong tin nhung nguoi thue phim do
+
+SELECT TOP 3 Rent1.IdNguoiDung, COUNT(Rent1.IdNguoiDung) AS soLuongNguoiThue
+FROM Rent1
+inner join Users1 ON Users1.IdNguoiDung = Rent1.IdNguoiDung
+GROUP BY Rent1.IdNguoiDung
+ORDER BY soLuongNguoiThue DESC 
+
+--b. Tinh tong doanh thu cua 1 bo phim trong 2 ngay
+SELECT DISTINCT Movie1.IdPhim, Movie1.tenPhim, Rent1.giaThue1Ngay*2*(COUNT(Rent1.IdNguoiDung)) AS tongdoanhThu2Ngay
+FROM Movie1
+inner join Rent1 on Movie1.IdPhim = Rent1.IdPhim
+
+--c. Lay ra top 3 phim duoc thue nhieu nhat trong 1 ngay (VD: ngay 05/02/2023)
+SELECT TOP 3 Rent1.IdPhim, COUNT(Rent1.IdPhim) AS soLuongDuocThue
+FROM Rent1
+GROUP BY Rent1.IdPhim
+ORDER BY soLuongDuocThue DESC 
+
+--d. Hien thi danh sach tat ca thong tin phim
+SELECT DISTINCT * FROM Movie1
